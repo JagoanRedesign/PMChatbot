0b078f24a9f6249585d5d2259e324bfe6d948e8f
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-29 23:50:17 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,14 @@
 #-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>Type your query here..\nI'll respond to your query as earliest</code> 😉"
-    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
-    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+    WELCOME_TEXT = """**Hello {}, 
+Thank you for trying to contact meh 😬
+
+📞 You can contact my [Master](https://t.me/Animesh941) using this bot! 🎈
+🛎 If you're going to message me for the first time, please read the help menu from below...!
+
+Feel free to ask me related to my Bots and Educational Channels..!!
+ ➖➖➖➖➖➖➖➖➖➖➖➖
+__© Coded By Animesh Verma 👀__**"""
+    USER_DETAILS = "**🗒️ --Private Message From :--\n\nName : {} {}\nUser ID : {}\nUsername : @{}\nUser Scam : {}\nRestricted : {}\nStatus : {}\nDC ID : {}**"
+    PM_TXT_ATT = "**💬 Message from : {}\nName : {}**\n\n{}"
+    PM_MED_ATT = "**💬 Message from : {} \nName : {}**"
