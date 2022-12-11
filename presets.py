51a9656dd41976b1d4383854f95a4f17cc1e21b1
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-11 18:56:35 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,13 +1,8 @@
-#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
-    WELCOME_TEXT = """**Hello {}, 
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n
 Terima kasih telah menghubungi
-
-Ketik pesan anda di sini..
-Saya akan menanggapi pesan anda secepat mungkin
-
-➖➖➖➖➖➖➖➖➖➖➖➖
-__ Bot by @PesertaGrup__**"""
-    USER_DETAILS = "**🗒️ --Private Message From :--\n\nName : {} {}\nUser ID : {}\nUsername : @{}\nUser Scam : {}\nRestricted : {}\nStatus : {}\nDC ID : {}**"
-    PM_TXT_ATT = "**💬 Message from : {}\nName : {}**\n\n{}"
-    PM_MED_ATT = "**💬 Message from : {} \nName : {}**"
+Ketik pesan anda di sini..\n
+Saya akan menanggapi pesan anda secepat mungkin"
+    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>id:</b> {}\n<b>Name:</b> <pre>{}</pre>\n\n{}"
+    PM_MED_ATT = "<b>id:</b> <pre>{}</pre> \n<b>Name:</b> {}"
