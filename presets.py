e860a26130ad77889e626e90e9cb7c6e6c5bc511
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-11 19:01:46 +0700

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,8 +1,5 @@
 class Presets(object):
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n
-Terima kasih telah menghubungi
-Ketik pesan anda di sini..\n
-Saya akan menanggapi pesan anda secepat mungkin"
+    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>Type your query here..\nI'll respond to your query as earliest</code> 😉"
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>id:</b> {}\n<b>Name:</b> <pre>{}</pre>\n\n{}"
     PM_MED_ATT = "<b>id:</b> <pre>{}</pre> \n<b>Name:</b> {}"
