8d127ba5b447c402346b2b6cfa089c7c9a46cd4e
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-11 19:30:03 +0700

    Update pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -1,3 +1,4 @@
+#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 import os
 
 from pyrogram import Client, filters
@@ -18,8 +19,7 @@ async def pm_text(bot, message):
     reference_id = int(message.chat.id)
     await bot.send_message(
         chat_id=Config.ADMIN,
-        text=Presets.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
-        parse_mode="html"
+        text=Presets.PM_TXT_ATT.format(reference_id, info.first_name, message.text)
     )
 
 
@@ -34,8 +34,7 @@ async def pm_media(bot, message):
         chat_id=Config.ADMIN,
         from_chat_id=message.chat.id,
         message_id=message.message_id,
-        caption=Presets.PM_MED_ATT.format(reference_id, info.first_name),
-        parse_mode="html"
+        caption=Presets.PM_MED_ATT.format(reference_id, info.first_name)
     )
 
 
@@ -58,7 +57,6 @@ async def reply_text(bot, message):
         )
 
 
-
 @Client.on_message(filters.user(Config.ADMIN) & filters.media)
 async def replay_media(bot, message):
     reference_id = True
@@ -75,6 +73,5 @@ async def replay_media(bot, message):
         await bot.copy_message(
             chat_id=int(reference_id),
             from_chat_id=message.chat.id,
-            message_id=message.message_id,
-            parse_mode="html"
+            message_id=message.message_id
         )
